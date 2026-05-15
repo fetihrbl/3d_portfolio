28300dba18a484a37585f8e3d22a3ffcdbdbadb7
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2026-05-15 07:06:51 -0700

    Implemen Feature Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import LogoSection from "./components/LogoSection";
 import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import AppShowcase from "./sections/ShowecaseSection";
 
@@ -9,6 +11,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <AppShowcase />
+      <LogoSection />
+      <FeatureCards />
     </>
   )
 };
